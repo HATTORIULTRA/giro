2bf7bf72b38dac759d3d24689f0d03abcf33b323
HATTORIULTRA <throwbvck@gmail.com>
2025-08-20 07:48:47 +0500

    feat: routes 1

--- src/features/board/board.page.tsx
+++ src/features/board/board.page.tsx
@@ -1,7 +1,9 @@
 import {useParams} from "react-router-dom";
 import {type PathParams, ROUTES} from "@/shared/model/routes.ts";
 
-export function BoardPage() {
+function BoardPage() {
   const params = useParams<PathParams[typeof ROUTES.BOARD]>()
   return <div>Board page {params.boardId}</div>;
 }
+
+export const Component = BoardPage;
--- src/features/boards-list/index.tsx
+++ src/features/boards-list/index.tsx
@@ -1,3 +1,8 @@
+import {href, Link} from "react-router-dom";
+import {ROUTES} from "@/shared/model/routes.ts";
+
 export function BoardsListPage() {
-  return <div>Boards list</div>;
+  return <div>Boards list
+    <Link to={href(ROUTES.BOARDS, {boardID: "1"})}>Board 1</Link>
+  </div>;
 }
